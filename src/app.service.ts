94fad0f6c63e4ef5dfad4dbba267da904fc2bb82
Pankaj Kumar <85155188+PankajFB@users.noreply.github.com>
2024-03-11 23:14:33 +0530

    changed the response

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,10 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
-  }
+    return 'Hello to the world of nestjs';
+  };
+
+  getNewHello(): string {
+    return 'this is my first ever written controller in the nestJS with the typescirpt';
+  };
 }
