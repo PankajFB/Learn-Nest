82987f96c53df68809f24ab5fff077fd86ba380f
Pankaj Kumar <85155188+PankajFB@users.noreply.github.com>
2024-03-14 19:41:44 +0530

    fixed the ESlint issue

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -10,7 +10,7 @@ export class AppController {
     return this.appService.getHello();
   }
 
-  @Get(/Hello)
+  @Get("/Hello")
   getAbout(): string {
     return this.appService.getNewHello();
   }
